d3b8e1c1e332aac9c5683c2d2de4fc660436bf93
mukesh <mukeshmahajan501@gmail.com>
2020-09-13 21:46:21 +0530

    [Mukesh]Added:for player win,tie or change the move

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-class PlayingTicTacToe {
+class PlayingTTT {
 
 	// function to re-set the board.
 	public static void settingBoard(String[][] board) {
@@ -61,6 +61,66 @@ class PlayingTicTacToe {
 
 	}
 
+	public static boolean checkForWin(List<Integer> board) {
+		if (checkRowsForWin(board) || checkColumnsForWin(board) || checkDiagonalsForWin(board)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean checkRowsForWin(List<Integer> board) {
+		List<Integer> row0 = new ArrayList<>();
+		row0.add(1);
+		row0.add(2);
+		row0.add(3);
+		List<Integer> row1 = new ArrayList<>();
+		row1.add(4);
+		row1.add(5);
+		row1.add(6);
+		List<Integer> row2 = new ArrayList<>();
+		row2.add(7);
+		row2.add(8);
+		row2.add(9);
+		if (board.containsAll(row0) || board.containsAll(row1) || board.containsAll(row2)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean checkColumnsForWin(List<Integer> board) {
+		List<Integer> col0 = new ArrayList<>();
+		col0.add(1);
+		col0.add(4);
+		col0.add(7);
+		List<Integer> col1 = new ArrayList<>();
+		col1.add(2);
+		col1.add(5);
+		col1.add(8);
+		List<Integer> col2 = new ArrayList<>();
+		col2.add(3);
+		col2.add(6);
+		col2.add(9);
+		if (board.containsAll(col0) || board.containsAll(col1) || board.containsAll(col2)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean checkDiagonalsForWin(List<Integer> board) {
+		List<Integer> dig0 = new ArrayList<>();
+		dig0.add(1);
+		dig0.add(5);
+		dig0.add(9);
+		List<Integer> dig1 = new ArrayList<>();
+		dig1.add(4);
+		dig1.add(5);
+		dig1.add(6);
+		if (board.containsAll(dig0) || board.containsAll(dig1)) {
+			return true;
+		}
+		return false;
+	}
+
 	public static void playingTheGame(String[][] board) {
 
 		Scanner sc = new Scanner(System.in);
@@ -79,11 +139,12 @@ class PlayingTicTacToe {
 
 		// setting winning condition for both opponents.
 		boolean playerResult = false;
+		boolean cpuResult = false;
 
 		// calling a function to choose the player and cpu symbol.
 		SymbolArray = choosingSymbol(toss, sc);
 		String playerSymbol = SymbolArray[0];
-
+		int checkIndex = 0;
 		// playing till either of the competitor win or tie.
 		do {
 			if (toss) {
@@ -95,13 +156,22 @@ class PlayingTicTacToe {
 				occupiedPosition.add(index);
 				PlayerPosition.add(index);
 				board = settingSymbol(board, playerSymbol, index);
-
+				playerResult = checkForWin(PlayerPosition);
+				if (playerResult) {
+					System.out.println("Player Wins");
+				}
+				checkIndex++;
 				toss = false;
 			} else {
 				toss = true;
 			}
 
-		} while (playerResult == false);
+			if (checkForWin(PlayerPosition) != true && checkIndex == 5) {
+				System.out.println("draw");
+				break;
+			}
+
+		} while (playerResult == false && cpuResult == false);
 
 		printBoard(board);
 
@@ -127,7 +197,7 @@ class TicTacToe {
 		// creating the board as an 3*3 array
 		String[][] board = new String[3][3];
 
-		PlayingTicTacToe.playingTheGame(board);
+		PlayingTTT.playingTheGame(board);
 
 	}
 }
